3ed82af114c20168f13647da28ced8c827ab24df
Mike Rapp & Jordan Johansen <student@epicodus.com>
2015-09-24 11:37:26 -0700

    Add isAnswered property to questions.

--- controllers/AnswersController.js
+++ controllers/AnswersController.js
@@ -2,6 +2,7 @@ ocarinaExperts.controller('AnswersCtrl', function AnswersCtrl($scope, $statePara
   $scope.question = UtilitiesFactory.findById(QuestionsFactory.questions, $stateParams.questionId)
   $scope.addAnswer = function() {
     $scope.question.answers.push({ answer: $scope.answer });
+    $scope.question.isAnswered = false;
     $scope.answer = null;
   }
 });
--- services/QuestionsFactory.js
+++ services/QuestionsFactory.js
@@ -2,7 +2,7 @@ ocarinaExperts.factory('QuestionsFactory', function QuestionsFactory() {
   var factory = {};
   factory.questions = [];
   factory.addQuestion = function() {
-    factory.questions.push({ question: factory.question, id: factory.questions.length + 1, answers: [] });
+    factory.questions.push({ question: factory.question, id: factory.questions.length + 1, answers: [], isAnswered: true });
     factory.question = null;
   };
 
